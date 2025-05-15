796d577ca9d7d507db4b8e6c44045e5dc9953c8d
john praesto <john_praesto@hotmail.com>
2025-05-15 17:56:17 +0200

    Added GetAllUsers and CreateUser

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SkillChallenge.Interfaces;
+using SkillChallenge.Models;
 
 namespace SkillChallenge.Controllers
 {
+    [ApiController]
+    [Route("/users")]
     public class UserController : ControllerBase
     {
         private readonly IUserRepository _userRepo;
@@ -12,7 +15,18 @@ namespace SkillChallenge.Controllers
             _userRepo = userRepo;
         }
 
-        //[HttpGet]
-        //public async Task<User> GetAllUsers() { }
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userRepo.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            var createdUser = await _userRepo.CreateUserAsync(user);
+            return Ok(user);
+        }
     }
 }
