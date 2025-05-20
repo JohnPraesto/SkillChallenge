9a7a19d1590119714241eb2b99f4a559783f2e93
john praesto <john_praesto@hotmail.com>
2025-05-20 14:32:56 +0200

    Created UpdateUserDTO

---
+++ SkillChallenge/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillChallenge.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
+    }
+}
