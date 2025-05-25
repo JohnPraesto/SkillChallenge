f6c3db7fa821483b254a0c1f2128624812c42b8e
john praesto <john_praesto@hotmail.com>
2025-05-25 17:21:54 +0200

    UserController needs rework adjusted to Identity Users (work in progress)

--- SkillChallenge/Controllers/UserController.cs
+++ SkillChallenge/Controllers/UserController.cs
@@ -1,77 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SkillChallenge.DTOs;
-using SkillChallenge.Interfaces;
-using SkillChallenge.Mappers;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using SkillChallenge.DTOs;
+//using SkillChallenge.Interfaces;
 
-namespace SkillChallenge.Controllers
-{
-    [ApiController]
-    [Route("/users")]
-    public class UserController : ControllerBase
-    {
-        private readonly IUserRepository _userRepo;
+//namespace SkillChallenge.Controllers
+//{
+//    [ApiController]
+//    [Route("/users")]
+//    public class UserController : ControllerBase
+//    {
+//        private readonly IUserRepository _userRepo;
 
-        public UserController(IUserRepository userRepo)
-        {
-            _userRepo = userRepo;
-        }
+//        public UserController(IUserRepository userRepo)
+//        {
+//            _userRepo = userRepo;
+//        }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllUsers()
-        {
-            var users = await _userRepo.GetAllUsersAsync();
-            return Ok(users);
-        }
+//        [HttpGet]
+//        public async Task<IActionResult> GetAllUsers()
+//        {
+//            var users = await _userRepo.GetAllUsersAsync();
+//            return Ok(users);
+//        }
 
-        [HttpGet("{id:int}")]
-        public async Task<IActionResult> GetUserById([FromRoute] int id)
-        {
-            var user = await _userRepo.GetUserByIdAsync(id);
+//        [HttpGet("{username:string}")]
+//        public async Task<IActionResult> GetUserByUsername([FromRoute] string username)
+//        {
+//            var user = await _userRepo.GetUserByUsernameAsync(username);
+//            if (user == null)
+//                return NotFound($"User with username '{username}' was not found in the database");
 
-            if (user == null)
-            {
-                return NotFound($"User with id {id} was not found in the database");
-            }
+//            return Ok(
+//                new DisplayUserDTO
+//                {
+//                    Id = user.Id,
+//                    UserName = user.UserName,
+//                    Email = user.Email,
+//                    ProfilePicture = user.ProfilePicture,
+//                }
+//            );
+//        }
 
-            return Ok(user);
-        }
+//        // A user should only be able to update his or her own profile
+//        // An admin should be able to update all accounts
+//        // This is not yet implemented
+//        [HttpPut]
+//        [Route("{id:int}")]
+//        public async Task<IActionResult> UpdateUser(
+//            [FromRoute] string id,
+//            [FromBody] UpdateUserDTO updateUser
+//        )
+//        {
+//            var user = await _userRepo.UpdateUserAsync(id, updateUser);
 
-        [HttpPost]
-        public async Task<IActionResult> CreateUser([FromBody] CreateUserDTO newUser)
-        {
-            var user = newUser.ToUserFromCreateUserDTO();
-            await _userRepo.CreateUserAsync(user);
-            return CreatedAtAction(nameof(GetUserById), new { id = user.UserId }, user);
-        }
+//            if (user == null)
+//            {
+//                return NotFound($"User with id {id} was not found in the database");
+//            }
 
-        [HttpPut]
-        [Route("{id:int}")]
-        public async Task<IActionResult> UpdateUser(
-            [FromRoute] int id,
-            [FromBody] UpdateUserDTO updateUser
-        )
-        {
-            var user = await _userRepo.UpdateUserAsync(id, updateUser);
+//            return Ok(user);
+//        }
 
-            if (user == null)
-            {
-                return NotFound($"User with id {id} was not found in the database");
-            }
+//        // A user should only be able to delete his or her own profile
+//        // An admin should be able to delete any account
+//        // This is not yet implemented
+//        [HttpDelete("{id:int}")]
+//        public async Task<IActionResult> DeleteUser([FromRoute] string id)
+//        {
+//            var user = await _userRepo.DeleteUserAsync(id);
 
-            return Ok(user);
-        }
+//            if (user == null)
+//            {
+//                return NotFound($"User with id {id} was not found in the database");
+//            }
 
-        [HttpDelete("{id:int}")]
-        public async Task<IActionResult> DeleteUser([FromRoute] int id)
-        {
-            var user = await _userRepo.DeleteUserAsync(id);
-
-            if (user == null)
-            {
-                return NotFound($"User with id {id} was not found in the database");
-            }
-
-            return NoContent();
-        }
-    }
-}
+//            return NoContent();
+//        }
+//    }
+//}
