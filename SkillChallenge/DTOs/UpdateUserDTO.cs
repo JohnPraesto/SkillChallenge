1f6c7a699d6cbd80d8ec2597b32920d9b34f95ba
john praesto <john_praesto@hotmail.com>
2025-05-25 17:24:57 +0200

    UpdateUserDTO not using Password anymore

--- SkillChallenge/DTOs/UpdateUserDTO.cs
+++ SkillChallenge/DTOs/UpdateUserDTO.cs
@@ -3,7 +3,9 @@
     public class UpdateUserDTO
     {
         public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+
+        //public string Password { get; set; } = string.Empty;
+        // använd "UserManager" istället... (?) för att ändra lösenord
         public string ProfilePicture { get; set; } = string.Empty;
     }
 }
