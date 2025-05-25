919143c18154e940f0da20d3de2d5813774235da
john praesto <john_praesto@hotmail.com>
2025-05-25 17:28:25 +0200

    New DTOs

---
+++ SkillChallenge/DTOs/DisplayUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillChallenge.DTOs
+{
+    public class DisplayUserDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
