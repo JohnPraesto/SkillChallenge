e96b268bc0d046bdf850b5f1c0f1d104177531da
Oskar Johansson <oskar.johanssons@hotmail.com>
2025-06-04 15:33:14 +0200

    Fixa prefix

--- SkillChallenge/Migrations/20250604132822_AddImagePath.cs
+++ SkillChallenge/Migrations/20250604132822_AddImagePath.cs
@@ -8,15 +8,9 @@ namespace SkillChallenge.Migrations
     public partial class AddImagePath : Migration
     {
         /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-
-        }
+        protected override void Up(MigrationBuilder migrationBuilder) { }
 
         /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-        }
+        protected override void Down(MigrationBuilder migrationBuilder) { }
     }
 }
