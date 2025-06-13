b6bd16fa32ceba32550e1bc42b67724020010ce7
john praesto <john_praesto@hotmail.com>
2025-06-13 06:53:04 +0200

    Removed TimePeriod from Challenge models

--- SkillChallenge/DTOs/Challenge/UpdateChallengeDTO.cs
+++ SkillChallenge/DTOs/Challenge/UpdateChallengeDTO.cs
@@ -4,7 +4,6 @@
     {
         public string ChallengeName { get; set; } = string.Empty;
         public DateTime EndDate { get; set; }
-        public DateTime TimePeriod { get; set; }
         public string Description { get; set; } = string.Empty;
         public bool IsPublic { get; set; }
         public int? SubCategoryId { get; set; }
